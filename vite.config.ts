6bba7e70d82b23792e842001b6e950460c3ab8b7
Arie <kelivdc@yahoo.com>
2026-06-18 21:10:36 +0700

    Tambah Ntro

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  nitro: {},
 })
 
 export default config
